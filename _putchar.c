c2ff95992d475828a652ee04dbbf4a4312beaf2a
habtom gebremariam araya <habtomaraya5@gmail.com>
2022-10-18 16:51:04 +0300

    commited by habtom

--- _putchar.c
+++ _putchar.c
@@ -22,10 +22,10 @@ int print_char(va_list types, char buffer[],
 /************************* PRINT A STRING *************************/
 /**
  * print_string - Prints a string
- * @types: List a of arguments
- * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
- * @width: get width.
+ * @types: parameter List a of arguments
+ * @buffer: parameter Buffer array to handle print
+ * @flags: parameter Calculates active flags
+ * @width: parameter get width.
  * @precision: Precision specification
  * @size: Size specifier
  * Return: Number of chars printed
